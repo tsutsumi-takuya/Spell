def36bcb90a3dade7a7547ddefefd519979fd970
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-06-29 06:50:53 +0000

    edit book(edit)(6/29)

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8e227acb97e12495140f4737b9b4183b860041fd5c141ac889d10d67382317d909f47bf42a5aab865b3698d3ccbdb09194fe69a671c61f5983ef0b1802306e1a'
