6bd3f83b74048bc98743545b8daffd326c39c392
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-07-07 08:07:49 +0000

    create search(7/7)

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,4 +2,16 @@ class Spot < ApplicationRecord
   belongs_to :user
 
   attachment :spot_image, destroy: false
+
+  def self.search(search)
+
+  	if
+  		search
+  		Spot.where(['spot_name LIKE ?', "%#{search}%"])
+
+  	else
+  		Spot.all
+  	end
+  end
+
 end
